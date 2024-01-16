f73f7693b28812ad81add15a9330e200c86548cd
Lionel Cancellieri <lio2014vt@gmail.com>
2024-01-16 13:16:35 -0300

    UPDATED: Inicio del footer: NOTEBOOK

--- src/app/shared/primeng/primeng.module.ts
+++ src/app/shared/primeng/primeng.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { MenuModule } from 'primeng/menu';
 import { MenubarModule } from 'primeng/menubar';
-
+import { DividerModule } from 'primeng/divider';
 
 
 
 @NgModule({
   exports: [
     MenuModule,
-    MenubarModule
+    MenubarModule,
+    DividerModule
   ]
 })
 export class PrimengModule { }
