9dbd03a1aba2e8c14c58e442763c65e82ad73d0f
lionel <lio2014vt@gmail.com>
2024-02-02 00:08:33 -0300

    UPDATED: Terminamos por ahora la logica de nuestros servicios: PC

--- src/app/client/pages/proyectos/proyectos.component.ts
+++ src/app/client/pages/proyectos/proyectos.component.ts
@@ -10,11 +10,13 @@ import { Servicio } from '../../models/proyecto.model';
 })
 export class ProyectosComponent implements OnInit, OnDestroy {
 
+
+
   constructor(
     private proyectosService: ProyectosService,
     private route: ActivatedRoute,
     private cd: ChangeDetectorRef
-    ) {}
+  ) { }
 
   selectedProject: string | null = null;
   servicios: Servicio[] | undefined;
@@ -22,27 +24,45 @@ export class ProyectosComponent implements OnInit, OnDestroy {
   fullscreen: boolean = false;
   activeIndex: number = 0;
   onFullScreenListener: any;
+  proyectosClass: string = 'proyectos';
 
   @ViewChild('galleria') galleria: Galleria | undefined;
 
   responsiveOptions: any[] = [
-      {
-          breakpoint: '1024px',
-          numVisible: 5
-      },
-      {
-          breakpoint: '768px',
-          numVisible: 3
-      },
-      {
-          breakpoint: '560px',
-          numVisible: 1
-      }
+    {
+      breakpoint: '1024px',
+      numVisible: 5
+    },
+    {
+      breakpoint: '768px',
+      numVisible: 3
+    },
+    {
+      breakpoint: '560px',
+      numVisible: 1
+    }
   ];
 
   ngOnInit() {
 
+    this.route.url.subscribe(segments => {
+      // Verificar si estamos en la página de proyectos
+
+      const isProyectosPage = segments.length === 0 || segments.some(segment => segment.path === 'proyectos');
+
+      if (!isProyectosPage) {
+        this.proyectosClass = 'individual-proyecto';
+      } else {
+        this.proyectosClass = 'proyectos-page';
+      }
+
+      // console.log('Is Proyectos Page:', isProyectosPage);
+      // console.log('Proyectos Class:', this.proyectosClass);
+    });
+
+
     this.route.paramMap.subscribe(async (params) => {
+      
       this.selectedProject = params.get('project');
       // console.log('Selected Project: proyectos.component.ts', this.selectedProject);
 
@@ -59,8 +79,9 @@ export class ProyectosComponent implements OnInit, OnDestroy {
 
       this.bindDocumentListeners();
     });
-
   }
+
+
 
   getDescripcionOpciones(opciones: { [key: string]: string }): string[] {
     return Object.values(opciones);
@@ -68,85 +89,85 @@ export class ProyectosComponent implements OnInit, OnDestroy {
 
   onThumbnailButtonClick() {
     this.showThumbnails = !this.showThumbnails;
-}
+  }
 
-toggleFullScreen() {
+  toggleFullScreen() {
     if (this.fullscreen) {
-        this.closePreviewFullScreen();
+      this.closePreviewFullScreen();
     } else {
-        this.openPreviewFullScreen();
+      this.openPreviewFullScreen();
     }
 
     this.cd.detach();
-}
+  }
 
-openPreviewFullScreen() {
+  openPreviewFullScreen() {
     let elem = this.galleria?.element.nativeElement.querySelector('.p-galleria');
     if (elem.requestFullscreen) {
-        elem.requestFullscreen();
+      elem.requestFullscreen();
     } else if (elem['mozRequestFullScreen']) {
-        /* Firefox */
-        elem['mozRequestFullScreen']();
+      /* Firefox */
+      elem['mozRequestFullScreen']();
     } else if (elem['webkitRequestFullscreen']) {
-        /* Chrome, Safari & Opera */
-        elem['webkitRequestFullscreen']();
+      /* Chrome, Safari & Opera */
+      elem['webkitRequestFullscreen']();
     } else if (elem['msRequestFullscreen']) {
-        /* IE/Edge */
-        elem['msRequestFullscreen']();
+      /* IE/Edge */
+      elem['msRequestFullscreen']();
     }
-}
+  }
 
-onFullScreenChange() {
+  onFullScreenChange() {
     this.fullscreen = !this.fullscreen;
     this.cd.detectChanges();
     this.cd.reattach();
-}
-
-
-closePreviewFullScreen() {
-  if (document.exitFullscreen) {
-    document.exitFullscreen();
-  } else if ((document as any).mozCancelFullScreen) {
-    /* Firefox */
-    (document as any).mozCancelFullScreen();
-  } else if ((document as any).webkitExitFullscreen) {
-    /* Chrome, Safari & Opera */
-    (document as any).webkitExitFullscreen();
-  } else if ((document as any).msExitFullscreen) {
-    /* IE/Edge */
-    (document as any).msExitFullscreen();
   }
-}
+
+
+  closePreviewFullScreen() {
+    if (document.exitFullscreen) {
+      document.exitFullscreen();
+    } else if ((document as any).mozCancelFullScreen) {
+      /* Firefox */
+      (document as any).mozCancelFullScreen();
+    } else if ((document as any).webkitExitFullscreen) {
+      /* Chrome, Safari & Opera */
+      (document as any).webkitExitFullscreen();
+    } else if ((document as any).msExitFullscreen) {
+      /* IE/Edge */
+      (document as any).msExitFullscreen();
+    }
+  }
 
 
 
-bindDocumentListeners() {
+  bindDocumentListeners() {
     this.onFullScreenListener = this.onFullScreenChange.bind(this);
     document.addEventListener('fullscreenchange', this.onFullScreenListener);
     document.addEventListener('mozfullscreenchange', this.onFullScreenListener);
     document.addEventListener('webkitfullscreenchange', this.onFullScreenListener);
     document.addEventListener('msfullscreenchange', this.onFullScreenListener);
-}
+  }
 
-unbindDocumentListeners() {
+  unbindDocumentListeners() {
     document.removeEventListener('fullscreenchange', this.onFullScreenListener);
     document.removeEventListener('mozfullscreenchange', this.onFullScreenListener);
     document.removeEventListener('webkitfullscreenchange', this.onFullScreenListener);
     document.removeEventListener('msfullscreenchange', this.onFullScreenListener);
     this.onFullScreenListener = null;
-}
+  }
 
-ngOnDestroy() {
+  ngOnDestroy() {
     this.unbindDocumentListeners();
-}
+  }
 
-galleriaClass() {
+  galleriaClass() {
     return `custom-galleria ${this.fullscreen ? 'fullscreen' : ''}`;
-}
+  }
 
-fullScreenIcon() {
+  fullScreenIcon() {
     return `pi ${this.fullscreen ? 'pi-window-minimize' : 'pi-window-maximize'}`;
-}
+  }
 
 
 }
