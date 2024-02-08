dc35888276edd2a21b2894a8fcf2820b3898f790
Lionel Cancellieri <lio2014vt@gmail.com>
2024-02-08 11:37:57 -0300

    UPDATED: Seguimos trabajando en los proyectos: NOTEBOOK

--- src/app/client/services/proyectos.service.ts
+++ src/app/client/services/proyectos.service.ts
@@ -6,6 +6,7 @@ import { Proyecto } from '../models/proyecto.model';
   providedIn: 'root',
 })
 export class ProyectosService {
+  private alt_description = 'Venado Tuerto - Provincia de Santa Fe';
   private selectedProjectSource = new BehaviorSubject<string | null>(null);
   selectedProject$ = this.selectedProjectSource.asObservable();
 
@@ -15,6 +16,31 @@ export class ProyectosService {
 
   getProyectos(): Promise<Proyecto[]> {
     const proyectos: Proyecto[] = [
+
+      {
+        ruta: 'alvear-1065',
+        nombre: 'ALVEAR 1065',
+        descripcion: {
+          opciones: {
+            option1: 'Remodelación de vivienda',
+            option2: 'Construcción de nuevo baño y lavadero',
+            option3: 'Reparación de cañerias de gas y presentación en litoral gas',
+            option4: 'Mantenimiento y nuevas cañerias de agua y cloacas',
+            option5: 'Arreglos y nuevas instalaciones eléctricas',
+          },
+        },
+        imagenes: [
+          { itemImageSrc: 'assets/imagenes/proyectos/alvear-1065/galeria/img-1.jpg', thumbnailImageSrc: 'assets/imagenes/proyectos/alvear-1065/small/img-1.jpg', alt: this.alt_description, title: 'Alvear 1065' },
+          { itemImageSrc: 'assets/imagenes/proyectos/alvear-1065/galeria/img-2.jpg', thumbnailImageSrc: 'assets/imagenes/proyectos/alvear-1065/small/img-2.jpg', alt: this.alt_description, title: 'Alvear 1065' },
+          { itemImageSrc: 'assets/imagenes/proyectos/alvear-1065/galeria/img-3.jpg', thumbnailImageSrc: 'assets/imagenes/proyectos/alvear-1065/small/img-3.jpg', alt: this.alt_description, title: 'Alvear 1065' },
+          { itemImageSrc: 'assets/imagenes/proyectos/alvear-1065/galeria/img-4.jpg', thumbnailImageSrc: 'assets/imagenes/proyectos/alvear-1065/small/img-4.jpg', alt: this.alt_description, title: 'Alvear 1065' },
+          { itemImageSrc: 'assets/imagenes/proyectos/alvear-1065/galeria/img-5.jpg', thumbnailImageSrc: 'assets/imagenes/proyectos/alvear-1065/small/img-5.jpg', alt: this.alt_description, title: 'Alvear 1065' },
+          { itemImageSrc: 'assets/imagenes/proyectos/alvear-1065/galeria/img-6.jpg', thumbnailImageSrc: 'assets/imagenes/proyectos/alvear-1065/small/img-6.jpg', alt: this.alt_description, title: 'Alvear 1065' },
+          { itemImageSrc: 'assets/imagenes/proyectos/alvear-1065/galeria/img-7.jpg', thumbnailImageSrc: 'assets/imagenes/proyectos/alvear-1065/small/img-7.jpg', alt: this.alt_description, title: 'Alvear 1065' },
+          { itemImageSrc: 'assets/imagenes/proyectos/alvear-1065/galeria/img-8.jpg', thumbnailImageSrc: 'assets/imagenes/proyectos/alvear-1065/small/img-8.jpg', alt: this.alt_description, title: 'Alvear 1065' },
+          { itemImageSrc: 'assets/imagenes/proyectos/alvear-1065/galeria/img-9.jpg', thumbnailImageSrc: 'assets/imagenes/proyectos/alvear-1065/small/img-8.jpg', alt: this.alt_description, title: 'Alvear 1065' },
+        ],
+      },
       {
         ruta: 'junin-2816',
         nombre: 'JUNIN 2816',
@@ -25,40 +51,48 @@ export class ProyectosService {
           },
         },
         imagenes: [
-          { itemImageSrc: 'assets/imagenes/proyectos/junin-2816/galeria/img-1.jpg', thumbnailImageSrc: 'assets/imagenes/proyectos/junin-2816/small/img-1.jpg', alt: 'Venado Tuerto - Santa Fe', title: 'Junin 2816' },
-          { itemImageSrc: 'assets/imagenes/proyectos/junin-2816/galeria/img-2.jpg', thumbnailImageSrc: 'assets/imagenes/proyectos/junin-2816/small/img-2.jpg', alt: 'Venado Tuerto - Santa Fe', title: 'Junin 2816' },
-          { itemImageSrc: 'assets/imagenes/proyectos/junin-2816/galeria/img-3.jpg', thumbnailImageSrc: 'assets/imagenes/proyectos/junin-2816/small/img-3.jpg', alt: 'Venado Tuerto - Santa Fe', title: 'Junin 2816' },
-          { itemImageSrc: 'assets/imagenes/proyectos/junin-2816/galeria/img-4.jpg', thumbnailImageSrc: 'assets/imagenes/proyectos/junin-2816/small/img-4.jpg', alt: 'Venado Tuerto - Santa Fe', title: 'Junin 2816' },
-          { itemImageSrc: 'assets/imagenes/proyectos/junin-2816/galeria/img-5.jpg', thumbnailImageSrc: 'assets/imagenes/proyectos/junin-2816/small/img-5.jpg', alt: 'Venado Tuerto - Santa Fe', title: 'Junin 2816' },
-          { itemImageSrc: 'assets/imagenes/proyectos/junin-2816/galeria/img-6.jpg', thumbnailImageSrc: 'assets/imagenes/proyectos/junin-2816/small/img-6.jpg', alt: 'Venado Tuerto - Santa Fe', title: 'Junin 2816' },
-          { itemImageSrc: 'assets/imagenes/proyectos/junin-2816/galeria/img-7.jpg', thumbnailImageSrc: 'assets/imagenes/proyectos/junin-2816/small/img-7.jpg', alt: 'Venado Tuerto - Santa Fe', title: 'Junin 2816' },
-          { itemImageSrc: 'assets/imagenes/proyectos/junin-2816/galeria/img-8.jpg', thumbnailImageSrc: 'assets/imagenes/proyectos/junin-2816/small/img-8.jpg', alt: 'Venado Tuerto - Santa Fe', title: 'Junin 2816' },
-          { itemImageSrc: 'assets/imagenes/proyectos/junin-2816/galeria/img-9.jpg', thumbnailImageSrc: 'assets/imagenes/proyectos/junin-2816/small/img-9.jpg', alt: 'Venado Tuerto - Santa Fe', title: 'Junin 2816' },
-          { itemImageSrc: 'assets/imagenes/proyectos/junin-2816/galeria/img-10.jpg', thumbnailImageSrc: 'assets/imagenes/proyectos/junin-2816/small/img-10.jpg', alt: 'Venado Tuerto - Santa Fe', title: 'Junin 2816' },
-          { itemImageSrc: 'assets/imagenes/proyectos/junin-2816/galeria/img-11.jpg', thumbnailImageSrc: 'assets/imagenes/proyectos/junin-2816/small/img-11.jpg', alt: 'Venado Tuerto - Santa Fe', title: 'Junin 2816' },
-          { itemImageSrc: 'assets/imagenes/proyectos/junin-2816/galeria/img-12.jpg', thumbnailImageSrc: 'assets/imagenes/proyectos/junin-2816/small/img-12.jpg', alt: 'Venado Tuerto - Santa Fe', title: 'Junin 2816' },
-          { itemImageSrc: 'assets/imagenes/proyectos/junin-2816/galeria/img-13.jpg', thumbnailImageSrc: 'assets/imagenes/proyectos/junin-2816/small/img-13.jpg', alt: 'Venado Tuerto - Santa Fe', title: 'Junin 2816' },
-          { itemImageSrc: 'assets/imagenes/proyectos/junin-2816/galeria/img-14.jpg', thumbnailImageSrc: 'assets/imagenes/proyectos/junin-2816/small/img-14.jpg', alt: 'Venado Tuerto - Santa Fe', title: 'Junin 2816' },
+          { itemImageSrc: 'assets/imagenes/proyectos/junin-2816/galeria/img-1.jpg', thumbnailImageSrc: 'assets/imagenes/proyectos/junin-2816/small/img-1.jpg', alt: this.alt_description, title: 'Junin 2816' },
+          { itemImageSrc: 'assets/imagenes/proyectos/junin-2816/galeria/img-2.jpg', thumbnailImageSrc: 'assets/imagenes/proyectos/junin-2816/small/img-2.jpg', alt: this.alt_description, title: 'Junin 2816' },
+          { itemImageSrc: 'assets/imagenes/proyectos/junin-2816/galeria/img-3.jpg', thumbnailImageSrc: 'assets/imagenes/proyectos/junin-2816/small/img-3.jpg', alt: this.alt_description, title: 'Junin 2816' },
+          { itemImageSrc: 'assets/imagenes/proyectos/junin-2816/galeria/img-4.jpg', thumbnailImageSrc: 'assets/imagenes/proyectos/junin-2816/small/img-4.jpg', alt: this.alt_description, title: 'Junin 2816' },
+          { itemImageSrc: 'assets/imagenes/proyectos/junin-2816/galeria/img-5.jpg', thumbnailImageSrc: 'assets/imagenes/proyectos/junin-2816/small/img-5.jpg', alt: this.alt_description, title: 'Junin 2816' },
+          { itemImageSrc: 'assets/imagenes/proyectos/junin-2816/galeria/img-6.jpg', thumbnailImageSrc: 'assets/imagenes/proyectos/junin-2816/small/img-6.jpg', alt: this.alt_description, title: 'Junin 2816' },
+          { itemImageSrc: 'assets/imagenes/proyectos/junin-2816/galeria/img-7.jpg', thumbnailImageSrc: 'assets/imagenes/proyectos/junin-2816/small/img-7.jpg', alt: this.alt_description, title: 'Junin 2816' },
+          { itemImageSrc: 'assets/imagenes/proyectos/junin-2816/galeria/img-8.jpg', thumbnailImageSrc: 'assets/imagenes/proyectos/junin-2816/small/img-8.jpg', alt: this.alt_description, title: 'Junin 2816' },
+          { itemImageSrc: 'assets/imagenes/proyectos/junin-2816/galeria/img-9.jpg', thumbnailImageSrc: 'assets/imagenes/proyectos/junin-2816/small/img-9.jpg', alt: this.alt_description, title: 'Junin 2816' },
+          { itemImageSrc: 'assets/imagenes/proyectos/junin-2816/galeria/img-10.jpg', thumbnailImageSrc: 'assets/imagenes/proyectos/junin-2816/small/img-10.jpg', alt: this.alt_description, title: 'Junin 2816' },
+          { itemImageSrc: 'assets/imagenes/proyectos/junin-2816/galeria/img-11.jpg', thumbnailImageSrc: 'assets/imagenes/proyectos/junin-2816/small/img-11.jpg', alt: this.alt_description, title: 'Junin 2816' },
+          { itemImageSrc: 'assets/imagenes/proyectos/junin-2816/galeria/img-12.jpg', thumbnailImageSrc: 'assets/imagenes/proyectos/junin-2816/small/img-12.jpg', alt: this.alt_description, title: 'Junin 2816' },
+          { itemImageSrc: 'assets/imagenes/proyectos/junin-2816/galeria/img-13.jpg', thumbnailImageSrc: 'assets/imagenes/proyectos/junin-2816/small/img-13.jpg', alt: this.alt_description, title: 'Junin 2816' },
+          { itemImageSrc: 'assets/imagenes/proyectos/junin-2816/galeria/img-14.jpg', thumbnailImageSrc: 'assets/imagenes/proyectos/junin-2816/small/img-14.jpg', alt: this.alt_description, title: 'Junin 2816' },
         ],
       },
       {
-        ruta: 'alvear-1065',
-        nombre: 'ALVEAR 1065',
+        ruta: 'santafe-2189',
+        nombre: 'SANTA FE 2189',
         descripcion: {
           opciones: {
-            option1: 'Remodelación',
+            option1: 'Ejecución local comercial'
+
           },
         },
         imagenes: [
-          { itemImageSrc: 'assets/imagenes/proyectos/alvear-1065/galeria/img-1.jpg', thumbnailImageSrc: 'assets/imagenes/proyectos/alvear-1065/small/img-1.jpg', alt: 'Venado Tuerto - Santa Fe', title: 'Alvear 1065' },
-          { itemImageSrc: 'assets/imagenes/proyectos/alvear-1065/galeria/img-2.jpg', thumbnailImageSrc: 'assets/imagenes/proyectos/alvear-1065/small/img-2.jpg', alt: 'Venado Tuerto - Santa Fe', title: 'Alvear 1065' },
-          { itemImageSrc: 'assets/imagenes/proyectos/alvear-1065/galeria/img-3.jpg', thumbnailImageSrc: 'assets/imagenes/proyectos/alvear-1065/small/img-3.jpg', alt: 'Venado Tuerto - Santa Fe', title: 'Alvear 1065' },
-          { itemImageSrc: 'assets/imagenes/proyectos/alvear-1065/galeria/img-4.jpg', thumbnailImageSrc: 'assets/imagenes/proyectos/alvear-1065/small/img-4.jpg', alt: 'Venado Tuerto - Santa Fe', title: 'Alvear 1065' },
-          { itemImageSrc: 'assets/imagenes/proyectos/alvear-1065/galeria/img-5.jpg', thumbnailImageSrc: 'assets/imagenes/proyectos/alvear-1065/small/img-5.jpg', alt: 'Venado Tuerto - Santa Fe', title: 'Alvear 1065' },
-          { itemImageSrc: 'assets/imagenes/proyectos/alvear-1065/galeria/img-6.jpg', thumbnailImageSrc: 'assets/imagenes/proyectos/alvear-1065/small/img-6.jpg', alt: 'Venado Tuerto - Santa Fe', title: 'Alvear 1065' },
-          { itemImageSrc: 'assets/imagenes/proyectos/alvear-1065/galeria/img-7.jpg', thumbnailImageSrc: 'assets/imagenes/proyectos/alvear-1065/small/img-7.jpg', alt: 'Venado Tuerto - Santa Fe', title: 'Alvear 1065' },
-          { itemImageSrc: 'assets/imagenes/proyectos/alvear-1065/galeria/img-8.jpg', thumbnailImageSrc: 'assets/imagenes/proyectos/alvear-1065/small/img-8.jpg', alt: 'Venado Tuerto - Santa Fe', title: 'Alvear 1065' },
-          { itemImageSrc: 'assets/imagenes/proyectos/alvear-1065/galeria/img-9.jpg', thumbnailImageSrc: 'assets/imagenes/proyectos/alvear-1065/small/img-8.jpg', alt: 'Venado Tuerto - Santa Fe', title: 'Alvear 1065' },
+          { itemImageSrc: 'assets/imagenes/proyectos/santa-fe-2189/galeria/img-1.jpg', thumbnailImageSrc: 'assets/imagenes/proyectos/santa-fe-2189/small/img-1.jpg', alt: this.alt_description, title: 'Santa Fe 2189' },
+          { itemImageSrc: 'assets/imagenes/proyectos/santa-fe-2189/galeria/img-2.jpg', thumbnailImageSrc: 'assets/imagenes/proyectos/santa-fe-2189/small/img-2.jpg', alt: this.alt_description, title: 'Santa Fe 2189' },
+          { itemImageSrc: 'assets/imagenes/proyectos/santa-fe-2189/galeria/img-3.jpg', thumbnailImageSrc: 'assets/imagenes/proyectos/santa-fe-2189/small/img-3.jpg', alt: this.alt_description, title: 'Santa Fe 2189' },
+          { itemImageSrc: 'assets/imagenes/proyectos/santa-fe-2189/galeria/img-4.jpg', thumbnailImageSrc: 'assets/imagenes/proyectos/santa-fe-2189/small/img-4.jpg', alt: this.alt_description, title: 'Santa Fe 2189' },
+          // { itemImageSrc: 'assets/imagenes/proyectos/santa-fe-2189/galeria/img-5.jpg', thumbnailImageSrc: 'assets/imagenes/proyectos/santa-fe-2189/small/img-5.jpg', alt: 'Venado Tuerto - Santa Fe', title: 'Santa Fe 2189' },
+          { itemImageSrc: 'assets/imagenes/proyectos/santa-fe-2189/galeria/img-6.jpg', thumbnailImageSrc: 'assets/imagenes/proyectos/santa-fe-2189/small/img-6.jpg', alt: this.alt_description, title: 'Santa Fe 2189' },
+          { itemImageSrc: 'assets/imagenes/proyectos/santa-fe-2189/galeria/img-7.jpg', thumbnailImageSrc: 'assets/imagenes/proyectos/santa-fe-2189/small/img-7.jpg', alt: this.alt_description, title: 'Santa Fe 2189' },
+          { itemImageSrc: 'assets/imagenes/proyectos/santa-fe-2189/galeria/img-8.jpg', thumbnailImageSrc: 'assets/imagenes/proyectos/santa-fe-2189/small/img-8.jpg', alt: this.alt_description, title: 'Santa Fe 2189' },
+          { itemImageSrc: 'assets/imagenes/proyectos/santa-fe-2189/galeria/img-9.jpg', thumbnailImageSrc: 'assets/imagenes/proyectos/santa-fe-2189/small/img-9.jpg', alt: this.alt_description, title: 'Santa Fe 2189' },
+          { itemImageSrc: 'assets/imagenes/proyectos/santa-fe-2189/galeria/img-10.jpg', thumbnailImageSrc: 'assets/imagenes/proyectos/santa-fe-2189/small/img-10.jpg', alt: this.alt_description, title: 'Santa Fe 2189' },
+          { itemImageSrc: 'assets/imagenes/proyectos/santa-fe-2189/galeria/img-11.jpg', thumbnailImageSrc: 'assets/imagenes/proyectos/santa-fe-2189/small/img-11.jpg', alt: this.alt_description, title: 'Santa Fe 2189' },
+          { itemImageSrc: 'assets/imagenes/proyectos/santa-fe-2189/galeria/img-12.jpg', thumbnailImageSrc: 'assets/imagenes/proyectos/santa-fe-2189/small/img-12.jpg', alt: this.alt_description, title: 'Santa Fe 2189' },
+          { itemImageSrc: 'assets/imagenes/proyectos/santa-fe-2189/galeria/img-13.jpg', thumbnailImageSrc: 'assets/imagenes/proyectos/santa-fe-2189/small/img-13.jpg', alt: this.alt_description, title: 'Santa Fe 2189' },
+          { itemImageSrc: 'assets/imagenes/proyectos/santa-fe-2189/galeria/img-14.jpg', thumbnailImageSrc: 'assets/imagenes/proyectos/santa-fe-2189/small/img-14.jpg', alt: this.alt_description, title: 'Santa Fe 2189' },
+          { itemImageSrc: 'assets/imagenes/proyectos/santa-fe-2189/galeria/img-15.jpg', thumbnailImageSrc: 'assets/imagenes/proyectos/santa-fe-2189/small/img-15.jpg', alt: this.alt_description, title: 'Santa Fe 2189' },
+
         ],
       },
       // Agregar más servicios según sea necesario
