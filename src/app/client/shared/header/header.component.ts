f2acab864bea9aa1ffdd81ed362383c66b40c99b
lionel <lio2014vt@gmail.com>
2024-03-22 22:17:08 -0300

    Updated: Arreglamos estilos generales de la aplicacion: PC

--- src/app/client/shared/header/header.component.ts
+++ src/app/client/shared/header/header.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'shared-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
+
 
 }
