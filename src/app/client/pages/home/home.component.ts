1b13608d75dd485775644864beb91c92a849b05a
lionel <lio2014vt@gmail.com>
2024-03-27 01:24:11 -0300

    Updated: Avanzamos con la seccion Home: PC

--- src/app/client/pages/home/home.component.ts
+++ src/app/client/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HomeService } from '../../services/home.service';
 
 @Component({
   selector: 'app-home',
@@ -7,7 +8,30 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
 
- 
+  images: any[] | undefined;
+
+    responsiveOptions: any[] = [
+        {
+            breakpoint: '1024px',
+            numVisible: 5
+        },
+        {
+            breakpoint: '768px',
+            numVisible: 3
+        },
+        {
+            breakpoint: '560px',
+            numVisible: 1
+        }
+    ];
+
+    constructor(private homeService: HomeService) {}
+
+    ngOnInit() {
+        this.homeService.getImages().then((images) => {
+            this.images = images;
+        });
+    }
 
 
 }
---
+++ src/app/client/services/home.service.ts
@@ -0,0 +1,20 @@
+
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class HomeService {
+  getImages(): Promise<any[]> {
+
+    return Promise.resolve([
+
+      { itemImageSrc: 'assets/imagenes/home/galleria/img-1.jpg'},
+      { itemImageSrc: 'assets/imagenes/home/galleria/img-2.jpg'},
+      { itemImageSrc: 'assets/imagenes/home/galleria/img-3.jpg'},
+
+    ]);
+  }
+}
+
+
