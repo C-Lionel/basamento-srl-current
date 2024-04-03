f9e41bff8001a4b23dbac461eaaa8a8f5df35ef4
lionel <lio2014vt@gmail.com>
2024-04-03 01:18:46 -0300

    Cambiamos el loading-general a loading de nombre, queda un solo loading para toda la web: PC

--- src/app/client/components/components.module.ts
+++ src/app/client/components/components.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { GaleriaImagenesComponent } from './galeria-imagenes/galeria-imagenes.component';
-import { LoadingGeneralComponent } from './loading-general/loading-general.component';
+import { LoadingComponent } from './loading/loading.component';
 import { CardAnimatedComponent } from './card-animated/card-animated.component';
 import { ButtonInfoCardComponent } from './button-info-card/button-info-card.component';
 import { PrimengModule } from 'src/app/shared/primeng/primeng.module';
@@ -12,7 +12,7 @@ import { PrimengModule } from 'src/app/shared/primeng/primeng.module';
 @NgModule({
   declarations: [
     GaleriaImagenesComponent,
-    LoadingGeneralComponent,
+    LoadingComponent,
     CardAnimatedComponent,
     ButtonInfoCardComponent,
 
@@ -23,7 +23,7 @@ import { PrimengModule } from 'src/app/shared/primeng/primeng.module';
   ],
   exports: [
     GaleriaImagenesComponent,
-    LoadingGeneralComponent,
+    LoadingComponent,
     CardAnimatedComponent,
     ButtonInfoCardComponent,
   ]
