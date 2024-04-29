2d1b66350222c6f3b18bfc0d5788391eefe820d4
Lionel <lio2014vt@gmail.com>
2024-04-29 16:12:46 -0300

    Seguimos avanzando en la vista de contacto: PC-OFICINA

--- src/app/client/components/card-animated/card-animated.component.ts
+++ src/app/client/components/card-animated/card-animated.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-card-animated',
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrl: './card-animated.component.scss'
 })
 export class CardAnimatedComponent {
-
+  @Input() svgName: string = '';
+  @Input() title: string = '';
+  @Input() subtitle_1: string = '';
+  @Input() subtitle_2?: string = '';
+  @Input() subtitle_3?: string = '';
+  @Input() width: string = '15rem';
+  @Input() height: string = '20.625rem';
+  @Input() fontSize: string = '1.1em';
 }
