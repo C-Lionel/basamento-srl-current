188a142d32d5228a774a70e554b73c651b4d535a
Lionel <lio2014vt@gmail.com>
2024-05-01 16:41:30 -0300

    Actualizaciones generales: PC-OFICINA

--- src/app/client/components/card-animated/card-animated.component.ts
+++ src/app/client/components/card-animated/card-animated.component.ts
@@ -12,6 +12,9 @@ export class CardAnimatedComponent {
   @Input() subtitle_2?: string = '';
   @Input() subtitle_3?: string = '';
   @Input() width: string = '15rem';
-  @Input() height: string = '20.625rem';
+  @Input() height: string = '20rem';
   @Input() fontSize: string = '1.1em';
+  @Input() fontSizeSpan: string = '4rem';
+  @Input() top: string = '2.6em';
+  @Input() fontSizeText: string = '1.3rem';
 }
