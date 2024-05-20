898c8e0a61ea22b78f091b925930499e8574435b
Lionel <lio2014vt@gmail.com>
2024-05-20 13:39:14 -0300

    seguimos avanzando con el responsive del home, empezamos con la imagen: PC-OFICINA

--- src/app/client/pages/home/home.component.ts
+++ src/app/client/pages/home/home.component.ts
@@ -18,35 +18,21 @@ export class HomeComponent {
 
   @HostListener('window:scroll', [])
   onWindowScroll() {
-    if (window.innerWidth > 600 && window.scrollY > 360) {
-      this.showWhatsAppButton = true;
-    } else if (window.innerWidth <= 600) {
+    if (window.scrollY > 360) {
       this.showWhatsAppButton = true;
     } else {
       this.showWhatsAppButton = false;
     }
   }
 
-  @HostListener('window:resize', [])
-  onWindowResize() {
-    this.setWhatsAppButtonVisibility();
-  }
 
   @HostListener('window:resize', ['$event'])
   onResize(event: any) {
     this.checkScreenWidth(event.target.innerWidth);
   }
 
-  setWhatsAppButtonVisibility() {
-    if (window.innerWidth <= 600) {
-      this.showWhatsAppButton = true;
-    } else {
-      this.showWhatsAppButton = false;
-    }
-  }
-
   private checkScreenWidth(width: number) {
-    // Define el rango de resoluciones en las que deseas mostrar el fragmento de código
+
     if (width <= 600) {
       this.showFragment = true;
     } else {
@@ -60,7 +46,7 @@ export class HomeComponent {
       this.images = images;
     });
 
-    this.setWhatsAppButtonVisibility();
+    // this.setWhatsAppButtonVisibility();
 
     this.checkScreenWidth(window.innerWidth);
   }
