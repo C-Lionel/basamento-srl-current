da5099f8c929c8af625698d5b4215de0a5319efc
lionel <lio2014vt@gmail.com>
2024-05-22 00:21:53 -0300

    agrege un setTimeOut al boton de la vista de nosotros para el responsive: PC

--- src/app/client/pages/nosotros/nosotros.component.ts
+++ src/app/client/pages/nosotros/nosotros.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   templateUrl: './nosotros.component.html',
@@ -6,8 +7,22 @@ import { Component } from '@angular/core';
 })
 export class NosotrosComponent {
 
+  constructor(
+    private router: Router
+  ) {}
+
   restartVideo(videoPlayer: HTMLVideoElement) {
     videoPlayer.currentTime = 0;
+  }
+  navigateWithDelay(url: string) {
+    const screenWidth = window.innerWidth;
+    if (screenWidth <= 600) {
+      setTimeout(() => {
+        this.router.navigate([url]);
+      }, 300);
+    } else {
+      this.router.navigate([url]);
+    }
   }
 
 }
